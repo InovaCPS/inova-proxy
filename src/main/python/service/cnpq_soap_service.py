1d74ff88414a8451269011ae99f237480a66caa8
Lucas Nadalete <lucas.nadalete@focusnetworks.com.br>
2018-10-14 23:28:49 -0300

    XML mime-type implementation

--- src/main/python/service/cnpq_soap_service.py
+++ src/main/python/service/cnpq_soap_service.py
@@ -74,7 +74,7 @@ class CnpqSoapService:
         xmlin.close()
         if os.path.exists(zipfilename):
             shutil.rmtree(basedirpath + zipfilename)
-        return xmlcv.decode('iso-8859-1').encode('utf-8')
+        return bytearray(xmlcv, 'iso-8859-1').decode('utf-8')
 
     def get_json_cv(self, cpf):
         cv = {'cpf': cpf, 'identificador': self.get_identificador(cpf)}
